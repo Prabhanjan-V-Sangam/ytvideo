938fc68f0200e307493143e57076ae616e671556
PrabhanjanVS <prabhanjanv.sangam@gmail.com>
2025-03-16 20:41:51 +0530

    performed inital setup all clear deployed it on kind and running wihout errors.Next i will perform caching

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+NGINX_URL = "http://192.168.1.8:8080"
+
+@app.route("/")
+def index():
+    # Fetch the directory listing from Nginx
+    response = requests.get(NGINX_URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract video links
+    video_files = []
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if href.endswith((".mp4", ".webm", ".mkv", ".avi", ".mov")):
+            video_files.append(href)
+
+    return render_template("index.html", video_files=video_files, nginx_url=NGINX_URL)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
