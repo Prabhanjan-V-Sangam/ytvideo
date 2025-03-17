38ea22e5aa228d01a0865394070acce7f8a6a614
PrabhanjanVS <prabhanjanv.sangam@gmail.com>
2025-03-17 23:48:22 +0530

    idk why but 2nd commit

--- app.py
+++ app.py
@@ -1,36 +1,22 @@
 from flask import Flask, render_template, request
 import requests
-import redis
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-# Connect to Redis Cache
-redis_client = redis.Redis(host="redis-service", port=6379, decode_responses=True)
 
 NGINX_URL = "http://192.168.1.8:8081/videos/"  # Load videos via Nginx reverse proxy cache
 
 @app.route("/")
 def index():
-    cached_videos = redis_client.get("video_list")
+    response = requests.get("http://192.168.1.8:8080/")  # Fetch from original server
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    if cached_videos:
-        print("Cache HIT")
-        video_files = cached_videos.split(",")  # Redis stores as a string
-    else:
-        print("Cache MISS")
-        response = requests.get("http://192.168.1.8:8080/")  # Fetch from original server
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # Extract video links
-        video_files = [
-            link.get("href")
-            for link in soup.find_all("a")
-            if link.get("href").endswith((".mp4", ".webm", ".mkv", ".avi", ".mov"))
-        ]
-
-        # Store in Redis for 10 minutes
-        redis_client.setex("video_list", 600, ",".join(video_files))
+    # Extract video links
+    video_files = [
+        link.get("href")
+        for link in soup.find_all("a")
+        if link.get("href").endswith((".mp4", ".webm", ".mkv", ".avi", ".mov"))
+    ]
 
     return render_template("index.html", video_files=video_files, nginx_url=NGINX_URL)
 
